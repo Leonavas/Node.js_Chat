f8b195259a14c9c56905c1a83db5d633e9a37b23
roederick <rodrigoleonavas@gmail.com>
2012-07-12 12:53:33 -0300

    prettier interface and message sent on keypress

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ function start(route, handle) {
   }
 
   http.createServer(onRequest).listen(8888);
-  console.log("Server has started.");
+  console.info("Server has started.");
 }
 
 exports.start = start;
